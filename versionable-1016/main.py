e8301fb94cc996a5ef8815fc76378e67c9258d41
Michael Lorton <mlorton@castlighthealth.com>
2015-07-26 21:56:20 -0700

    added version-retrieving function

--- versionable-1016/main.py
+++ versionable-1016/main.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
+import sys
+import os
+sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+from flask import Flask, jsonify, session
+
+app = Flask(__name__)
+
+@app.route('/version/<commit>', methods=["GET"])
+def version(commit):
+    return app.make_response("""<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+  <head>
+    <base href="https://cdn.rawgit.com/Malvolio/versionable-demo/%s/public/"/>
+    <title>Demonstration of Versionable Concept</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.3/angular.min.js"></script>
+    <script src="app.js"></script>
+  </head>
+  <body ng-app="versionableDemo">
+    <div ng-include="'login.html'"></div>
+  </body>
+</html>""" % commit)
+
+
